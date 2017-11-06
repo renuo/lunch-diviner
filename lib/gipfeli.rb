cb69b533e79d45d07fed1e81407f18aa700f426c
Sebastian Rothmund <sebastian.rothmund@renuo.ch>
2017-11-06 10:51:12 +0100

    Extract memchache

---
+++ lib/gipfeli.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'dalli'
+
+class Gipfeli
+  def self.cache
+    Dalli::Client.new((ENV['MEMCACHIER_SERVERS'] || '').split(','),
+                      username: ENV['MEMCACHIER_USERNAME'],
+                      password: ENV['MEMCACHIER_PASSWORD'],
+                      failover: true,
+                      socket_timeout: 1.5,
+                      socket_failure_delay: 0.2)
+  end
+end
